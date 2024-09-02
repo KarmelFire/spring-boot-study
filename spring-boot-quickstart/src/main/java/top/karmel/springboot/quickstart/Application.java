64bae2f587d302c0cbd25400e4c40087fcfeb3f6
BC <bian2003chen@163.com>
2024-09-02 15:51:32 +0800

    quickstart模块创建完成

---
+++ spring-boot-quickstart/src/main/java/top/karmel/springboot/quickstart/Application.java
@@ -0,0 +1,18 @@
+package top.karmel.springboot.quickstart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @ClassName Application
+ * @Description TODO
+ * @Author BC
+ * @Date 2024/9/2 15:46
+ * @Version 1.0
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
